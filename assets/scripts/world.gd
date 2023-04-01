ea80e55d422bdd6f0b5f701d6e2b71db09c3d9d8
vinjarv <vinjar97@gmail.com>
2023-04-01 18:27:19 +0200

    Tesst

---
+++ assets/scripts/world.gd
@@ -0,0 +1,10 @@
+extends Node3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print("Hello World")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
