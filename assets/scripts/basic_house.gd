180b9c7fb27fc3b8f08c36a0f3e723788a5e8506
SanderJS <sander@skarmyr.no>
2023-04-02 18:21:47 +0200

    link between mouse click and unit spawn

--- assets/scripts/basic_house.gd
+++ assets/scripts/basic_house.gd
@@ -7,17 +7,17 @@ extends Node3D
 func _ready():
 	pass
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-#	if (_get_context_menu_clicked() == true):
-#		spawn_new_infected()
 
 func spawn_new_infected():
 	# character_basic for the pre-loaded one, or character_scene for the modular one in editor
 	var character = character_basic.instantiate()
 	var character_spawn_location = get_node("SpawnPath/SpawnLocation")
-	character_spawn_location.progress_rato = randf()
-	#_get_context_menu_clicked() = false
-	add_child(character)
+	character_spawn_location.progress_ratio = randf()
+	get_tree().get_root().get_node("World/Peasants").add_child(character)
+	character.global_position = character_spawn_location.global_position
+	character.click_position = character.position
+	print("Unit spawned!")
+	
