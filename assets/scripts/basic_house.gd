280767bf85314f73d6a368c0c5a8abb3604307a8
SanderJS <sander@skarmyr.no>
2023-04-02 15:28:54 +0200

    unfinished spawn of unit at random place in front of house

--- assets/scripts/basic_house.gd
+++ assets/scripts/basic_house.gd
@@ -1,11 +1,23 @@
 extends Node3D
 
+@export var character_scene: PackedScene
+@onready var character_basic = preload("res://assets/scenes/basic_infected_script.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+#	if (_get_context_menu_clicked() == true):
+#		spawn_new_infected()
+
+func spawn_new_infected():
+	# character_basic for the pre-loaded one, or character_scene for the modular one in editor
+	var character = character_basic.instantiate()
+	var character_spawn_location = get_node("SpawnPath/SpawnLocation")
+	character_spawn_location.progress_rato = randf()
+	#_get_context_menu_clicked() = false
+	add_child(character)
